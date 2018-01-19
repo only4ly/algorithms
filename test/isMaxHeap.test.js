81f083b92ee72641a0fdec403a372ae8ab4a51f8
zhangwei <935279748@qq.com>
2018-01-19 09:48:08 +0800

    add more isMaxHeap test case

--- test/isMaxHeap.test.js
+++ test/isMaxHeap.test.js
@@ -8,4 +8,13 @@ describe('isMaxHeap is function to judge an array is an maxHeap or not', functio
   it('shou return true', function () {
     assert.equal(isMaxHeap([3, 2]), true)
   })
+  it('shou return false', function () {
+    assert.equal(isMaxHeap([2, 3, 1]), false)
+  })
+  it('shou return false', function () {
+    assert.equal(isMaxHeap([27, 17, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0]), false)
+  })
+  it('shou return true', function () {
+    assert.equal(isMaxHeap([27, 17, 10, 16, 13, 9, 1, 5, 7, 12, 4, 8, 3, 0]), true)
+  })
 })
