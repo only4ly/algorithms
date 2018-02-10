de11faedce92899b6e9e80aec6590c78f77b86f8
zhangwei <935279748@qq.com>
2018-02-10 16:46:30 +0800

    +++ util fuc

--- functional/util.js
+++ functional/util.js
@@ -2,10 +2,20 @@ const _ = require('ramda')
 const reduce = _.curry((f, a, xs) => xs.reduce(f, a))
 const map = _.curry((f, arr) => arr.map(f))
 const add = _.curry((x, y) => x + y)
+const head = _.curry((x) => x[0])
+const last = _.curry((x) => x[x.length - 1])
 const replace = _.curry((what, replacement, x) => x.replace(what, replacement))
+const split = _.curry((s, str) => str.split(s))
+const filter = _.curry((f, arr) => arr.filter(f))
+const eq = _.curry((x, y) => x === y)
 module.exports = {
   reduce,
   map,
   add,
-  replace
+  replace,
+  head,
+  last,
+  split,
+  filter,
+  eq
 }
